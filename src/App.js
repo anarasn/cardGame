a286ec1ebc0afbfa2be3e4ac681289695ad4405b
Test User <luner-stone@mail.ru>
2022-06-27 17:27:07 +0300

    added images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import axios from 'axios';
-import {useEffect} from 'react';
+import { nanoid } from 'nanoid';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 const mainURL = "https://api.unsplash.com/photos/?client_id="
 
 function App() {
+  const [photos, setPhotos] = useState([])
   async function callToApi(){
-    const {data} = await axios.get(mainURL+process.env.REACT_APP_API_KEY)
-    console.log(data)
-  }
+  const page1 = `${mainURL}${process.env.REACT_APP_API_KEY}&page=${1}`
+  const page2 = `${mainURL}${process.env.REACT_APP_API_KEY}&page=${2}`
+  const [images1, images2] = await axios.all([axios.get(page1), axios.get(page2)])
+  let images =[
+    ...images1.data,
+    ...images1.data,
+    ...images2.data.slice(0,2),
+    ...images2.data.slice(0,2)
+  ]
+  images = images.map(img=>{
+    return{...img, unique: nanoid()}
+  })  
+  setPhotos(images)
+ }
   
   useEffect(()=>{
     callToApi()
@@ -16,6 +29,15 @@ function App() {
 
   return (
     <div className="App">  
+    {
+      photos.map(photo=>{
+        return(
+          <div className='single_card' key={photo.unique}>
+          <img src={photo.urls.thumb} alt={photo.description}/>
+          </div>
+        )
+      })
+    }
    
     </div>
   );
